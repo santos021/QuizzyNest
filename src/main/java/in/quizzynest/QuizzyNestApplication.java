b6e942329aa6472c4c2fcf3c2902c9b8e4de56d2
Santos Kumar Biswal <santosbiswal543@gmail.com>
2025-08-22 01:08:46 +0530

    ...

--- src/main/java/in/quizzynest/QuizzyNestApplication.java
+++ src/main/java/in/quizzynest/QuizzyNestApplication.java
@@ -1,5 +1,7 @@
 package in.quizzynest;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuizzyNestApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(QuizzyNestApplication.class, args);
 	}
 
